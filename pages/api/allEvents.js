df3037f79d4751c9b8a9666d733203ece9e2e982
Simon <simonjudd2@gmail.com>
2023-03-29 11:59:13 +0100

    fix(add try):

--- pages/api/allEvents.js
+++ pages/api/allEvents.js
@@ -1,21 +1,24 @@
-import clientPromise from "../../lib/mongodb"
 import { connectToDatabase } from "../../lib/mongodb"
 import { DB_NAME } from '../../src/constants/constants'
 
 export default async function handler(req, res) {
-
+  try {
     const { database } = await connectToDatabase();
     const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
 
     const solutions = await collection.find({})
-        .sort({
-            '_chain.valid_from': -1 // prefer latest
-        })
-        .toArray()
+      .sort({
+        '_chain.valid_from': -1 // prefer latest
+      })
+      .toArray()
 
-  if (solutions) {
-    return res.status(200).json({ 'DeathMachine': solutions });
-  } else {
-    return res.status(404).json({ 'DeathMachine': 'Not found' });
+    if (solutions) {
+      return res.status(200).json({ 'DeathMachine': solutions });
+    } else {
+      return res.status(404).json({ 'DeathMachine': 'Not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return res.status(500).json({ 'DeathMachine': 'Error fetching data' });
   }
 }
